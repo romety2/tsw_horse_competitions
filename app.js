e2b3af6d4d5f09e7d8c1eb496e3150515c90ae5e
romety2 <lmielewczyk@sigma.ug.edu.pl>
2016-05-24 09:41:16 +0200

    dodano polaczenie szyfrowane

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-/* jshint node: true */
+/* jshint node: true, esnext: true */
 var path = require('path');
 var express = require('express');
-var http = require('http');
+var fs = require('fs');
+var https = require('https');
 var app = express();
+var routes = require('./routes');
+
+const options = {
+  key: fs.readFileSync('certifications/key.pem'),
+  cert: fs.readFileSync('certifications/server.crt')
+};
 
 //app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
-
-var routes = require('./routes');
 
 app.get('/', routes.index);
 app.get('/kontakt', routes.kontakt);
@@ -20,8 +25,11 @@ app.use(require('serve-favicon')(__dirname + '/public/img/logo.ico'));
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function () {
+https.createServer(options, app).listen(app.get('port'), function () {
     console.log("Serwer nasłuchuje na porcie " + app.get('port'));
 });
+
+/*app.listen(app.get('port'), function () {
+    console.log("Serwer nasłuchuje na porcie " + app.get('port'));
+});*/
